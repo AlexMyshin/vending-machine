33e406328698a11f15145899661b02721e7c6690
Nicola Aitken <naitken22@gmail.com>
2014-09-15 20:49:31 +0100

    User can select item and pay in coins

---
+++ lib/user.rb
@@ -0,0 +1,18 @@
+class User
+
+attr_accessor :selection, :amount_paid
+
+def initialize
+	@selection = []
+	@amount_paid = []
+end
+
+def select(item)
+	@selection << item
+end
+
+def insert(coin)
+	@amount_paid << coin
+end
+
+end
